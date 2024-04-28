a9392fe9d8729624c8c53063094d42a81ec49459
himesaka3ness <134124455+himesaka3ness@users.noreply.github.com>
2024-04-28 15:21:40 +0900

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-  document.addEventListener('DOMContentLoaded', function() {
-    const menuIcon = document.querySelector('.menu-icon');
-    const headerRight = document.querySelector('.header-right');
-    
-    menuIcon.addEventListener('click', function() {
-      headerRight.style.display = headerRight.style.display === 'block' ? 'none' : 'block';
-    });
-  });
